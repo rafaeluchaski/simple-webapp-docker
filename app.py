9f94e4815ffecc669cfbccde59d97c15d6b46c6f
paulovigne <57726813+paulovigne@users.noreply.github.com>
2023-12-04 09:57:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import os
+import socket
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Bem Vindo ao Docker Build!"
+    return '<h1><p style="color:' + os.environ.get('COLOR', 'black') + '">Bem Vindo ao Docker Build!</p></h1>'
 
-@app.route('/comovai')
+@app.route('/id')
 def hello():
-    return 'Muito Bem!'
+    hostname = socket.gethostname()
+    ip_addr = socket.gethostbyname(hostname)
+    return '<h1>Hostname: ' + hostname + '<br>' + 'IP Address: ' + ip_addr + '</h1>'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8080)
